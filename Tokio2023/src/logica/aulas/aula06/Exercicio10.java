099a5821ac88abf383d3fe0157056b80b2de6e10
lannaleny <lannalenymelo@gmail.com>
2023-04-30 15:10:18 -0300

    Aula6

--- Tokio2023/src/logica/aulas/aula06/Exercicio10.java
+++ Tokio2023/src/logica/aulas/aula06/Exercicio10.java
@@ -6,7 +6,40 @@ public class Exercicio10 {
 
 	public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-
+      
+      System.out.println("Digite seu salario atual");
+      double salarioAtual = sc.nextDouble();
+      
+      System.out.println(" ");
+      
+      double aumentoSalario = 0;
+      if (salarioAtual <= 280.0 ) {
+        aumentoSalario = 0.2;
+	}else if (salarioAtual > 280 && salarioAtual <= 700) {
+		aumentoSalario = 0.15;
+	}else if (salarioAtual > 700 && salarioAtual <=1500) {
+		aumentoSalario = 0.10;
+	}else {
+		aumentoSalario = 0.05;
 	}
 
+      double aumento = (salarioAtual * aumentoSalario);
+      double novoSalario = (salarioAtual + aumento);
+      
+      
+      System.out.println("O salario era de : R$" + salarioAtual);
+      System.out.println("O percentual do aumento: " + (aumentoSalario * 100) + "%");
+      System.out.println("O valor do aumento: R$" + aumento );
+      System.out.println("O valor do salario depois do aumento: R$" + novoSalario);
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      sc.close();
+}
 }
--- Tokio2023/src/logica/aulas/aula06/Exercicio11.java
+++ Tokio2023/src/logica/aulas/aula06/Exercicio11.java
@@ -3,5 +3,28 @@ package logica.aulas.aula06;
 import java.util.Scanner;
 
 public class Exercicio11 {
-    Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		    	   
+       System.out.println("O código do estado de origem da carga de um caminhão,");
+       int num = sc.nextInt();
+			
+	   System.out.println("Digite o peso da carga do caminhao em toneladas");
+	   int toneladas = sc.nextInt();
+		    
+		System.out.println("Digite da codigo da carga");
+		int codigo = sc.nextInt();
+		
+		
+		int quilos = (toneladas * 1000);
+		
+		
+		
+		
+	   
+	   
+
+	}
+
 }
